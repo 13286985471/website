877ec597f7b7dd226ef0399293717570664e16d4
Anonymous <13286985471@163.com>
2019-12-25 16:26:11 +0800

    we

---
+++ site-gateway/src/main/java/it/world/gateway/SiteGateWayApplication.java
@@ -0,0 +1,13 @@
+package it.world.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class SiteGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteGateWayApplication.class, args);
+    }
+
+}
