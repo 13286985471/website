658ab03649e55583eb3ba62b1d9b23b28c4008b3
Anonymous <13286985471@163.com>
2020-01-03 15:49:30 +0800

    we

--- site-registry/src/main/java/it/world/eureka/SiteRegistryApplication.java
+++ site-registry/src/main/java/it/world/eureka/SiteRegistryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SiteEurekaApplication {
+public class SiteRegistryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SiteEurekaApplication.class, args);
+        SpringApplication.run(SiteRegistryApplication.class, args);
     }
 
 }
