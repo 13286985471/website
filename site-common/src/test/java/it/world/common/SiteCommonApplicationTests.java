93f49f7e822cf9e95a1a2c4870eae124aef7e7cd
Anonymous <13286985471@163.com>
2020-01-07 10:17:15 +0800

    we

---
+++ site-common/src/test/java/it/world/common/SiteCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package it.world.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
