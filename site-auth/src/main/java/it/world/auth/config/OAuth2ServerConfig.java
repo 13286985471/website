a878d51405f5ca395ce96fa01b503bce0fc41aa7
Anonymous <13286985471@163.com>
2020-01-18 22:50:48 +0800

    we

--- site-auth/src/main/java/it/world/auth/config/OAuth2ServerConfig.java
+++ site-auth/src/main/java/it/world/auth/config/OAuth2ServerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -58,6 +59,12 @@ public class OAuth2ServerConfig {
         @Override
         public AuthenticationManager authenticationManagerBean() throws Exception {
             return super.authenticationManagerBean();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .anyRequest().permitAll().and().logout().permitAll();//配置不需要登录验证
         }
     }
 
